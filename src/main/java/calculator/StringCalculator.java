c11764c57f25a1a620c93d96528d68cbdb0d796d
Vishwadeepsinh Gohil <gohilvishwadeepsinh2001@gmail.com>
2024-07-30 18:03:33 +0530

    Handled test cases with any amount of numbers and new lines

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,18 +3,26 @@ package calculator;
 public class StringCalculator {
 
 	 int add(String numbers) {
+		 
+		//If string is empty 
 		if (numbers.isEmpty()) {
 	        return 0;
 	    }
-	    String[] numberArray = numbers.split(",");
+		
+		//Splitting string at commas and new lines
+	    String[] numberArray = numbers.split(",|\n");
 	    int sum = 0;
+	    
 	    for (String number : numberArray) {
 	        try {
+	        	//Parsing and adding
 	            sum += Integer.parseInt(number);
 	        } catch (NumberFormatException e) {
 	            System.out.println("Failed to parse number: " + number);
 	        }
 	    }
+	    
+	    
 	    return sum;
 	}
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -10,14 +10,14 @@ public class StringCalculatorTest {
 
     // 1. For empty string, add method should return 0
     @Test
-    void emptyString() {
+    void empty_String() {
         int value = sc1.add("");
         Assertions.assertEquals(value, 0);
     }
 
     // 2. For single number string, add should return same number.
     @Test
-    void stringwithOnenumber() {
+    void string_with_Onenumber() {
         int value = sc1.add("1");
         Assertions.assertEquals(value, 1);
         value = sc1.add("90");
@@ -26,10 +26,26 @@ public class StringCalculatorTest {
 
     // 3. For two numbers string separated by comma, add method should return sum of both.
     @Test
-    void stringwithTwonumber() {
+    void string_with_Twonumbers() {
         int value = sc1.add("1,5");
         Assertions.assertEquals(value, 6);
         value = sc1.add("23,35");
         Assertions.assertEquals(value, 58);
+    }
+    
+    // 4. For handling strings with any amount of numbers.
+    @Test
+    void string_with_Anynumbers() {
+    	int value = sc1.add("1,5,34,56,98");
+    	Assertions.assertEquals(value, 194);
+    	value = sc1.add("100,1000,200,400,321,93");
+    	Assertions.assertEquals(value, 2114);
+    }
+    
+    // 5. String with \n for addition
+    @Test
+    void string_with_newLine() {
+    	int value = sc1.add("1\n2,3");
+    	Assertions.assertEquals(value, 6);
     }
 }
